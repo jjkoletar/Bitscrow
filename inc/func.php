a3d67b0a3e9a83276d10893bfef044f6b95f7f6d
Jeremy Koletar <jjkoletar@gmail.com>
2011-07-11 23:05:02 -0500

    Fix hawk's failure

--- inc/func.php
+++ inc/func.php
@@ -3,12 +3,12 @@ require_once('jsonRPCClient.php');
 require_once('config.php');
 
 function rpcConnect() {
-    $rpc = new jsonRPCClient('http://' . $config['rpcuser'] . ':' . $config['rpcpass'] . '@' $config['rpchost'] . ':' . $config['rpcport'] . '/');
+    $rpc = new jsonRPCClient('http://' . $config['rpcuser'] . ':' . $config['rpcpass'] . '@' . $config['rpchost'] . ':' . $config['rpcport'] . '/');
     return $rpc;
 }
 
 function configCheck() {
-    if(!isSet($config['dbhost']) || !isSet($config['dbname') || !isSet($config['dbuser') || !isSet($config['dbpass') || !isSet($config['dbprefix') || !isSet($config['rpcuser') || !isSet($config['rpcpassword') || !isSet($config['rpcport') || !isSet($config['rpchost')) {
+    if(!isSet($config['dbhost']) || !isSet($config['dbname']) || !isSet($config['dbuser']) || !isSet($config['dbpass']) || !isSet($config['dbprefix']) || !isSet($config['rpcuser']) || !isSet($config['rpcpassword']) || !isSet($config['rpcport']) || !isSet($config['rpchost'])) {
         return false;
     }
     else {
